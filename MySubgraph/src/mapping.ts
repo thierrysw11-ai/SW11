ae432cad62db652192d3972b5833fc1dda3c5b43
thierrysw11-ai <thierrysw11@gmail.com>
2025-10-04 17:11:20 +0100

    Add files via upload

---
+++ MySubgraph/src/mapping.ts
@@ -0,0 +1,12 @@
+import { Transfer } from '../generated/schema'
+import { Transfer as TransferEvent } from '../generated/USDC/USDC'
+
+export function handleTransfer(event: TransferEvent): void {
+  let entity = new Transfer(event.transaction.hash.toHex() + '-' + event.logIndex.toString())
+  entity.from = event.params.from
+  entity.to = event.params.to
+  entity.value = event.params.value
+  entity.blockNumber = event.block.number
+  entity.timestamp = event.block.timestamp
+  entity.save()
+}
